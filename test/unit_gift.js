47e8d5ab177dad1230d06e9bc7354e476383f963
ccousin <ccousin@digitalvirgo.com>
2016-04-25 17:53:36 +0200

    Beginning of unit testing

--- test/unit_gift.js
+++ test/unit_gift.js
@@ -1,27 +1,50 @@
 var assert = require('chai').assert;
-var Gift = require('../gift').Gift;
+var DBHandle = require('../database');
 var sinon = require('sinon');
 var Promise = require('bluebird');
 
-describe('Gift', function () {
-    it('should create a new gift if no gift from foo to bar exists', function (done) {
-        //setup
-        var gift = new Gift('foo', 'bar');
-        var giftCreate = Promise.resolve({get:sinon.stub().returns({insertion:'data'})});
-        var createNewGiftStub = sinon.stub().returns(giftCreate)
-        sinon.stub(gift, 'getDb').returns({
-            findAndCountAllGifts: sinon.stub().returns(Promise.resolve({count:0})),
-            createNewGift: createNewGiftStub
-        });
-        var replyCallback = sinon.spy();
-        sinon.stub(gift, 'compute_max_expiration').returns(new Date(0));
-        //action
-        gift.give(function(data){
-            //assert
+// Mocking creation
+var giftCreate = Promise.resolve({get:sinon.stub().returns({insertion:'data'})});
+var createNewGiftStub = sinon.stub().returns(giftCreate);
 
-            assert.deepEqual(createNewGiftStub.getCall(0).args, ['foo', 'bar', new Date(0)]);
-            assert.deepEqual(data, {"insertion": "data"});
+// Mocking gift lookup
+var fakeExistingGift = {};
+fakeExistingGift.count = 1;
+fakeExistingGift.rows = { 0: { get:sinon.stub().returns(new Date()) }};
+
+var findAllGiftsStub = sinon.stub();
+findAllGiftsStub.onCall(0).returns(Promise.resolve({count:0}));
+findAllGiftsStub.onCall(1).returns(Promise.resolve(fakeExistingGift));
+
+var dbStub = sinon.stub(DBHandle, 'DBHandle').returns({
+    findAllGifts: findAllGiftsStub,
+    createNewGift: createNewGiftStub
+});
+
+var Gift = require('../gift');
+
+describe('Gift', function () {
+    var clock = sinon.useFakeTimers();
+
+    it('Sending no existing', function (done) {
+        Gift.give('foo', 'bar', function(data) {
+            expiration = new Date();
+            expiration.setDate(expiration.getDate() + 7);
+            assert.deepEqual(createNewGiftStub.getCall(0).args, ['foo', 'bar', expiration]);
+            assert.deepEqual(data, {'insertion': 'data'});
             done();
         });
     });
+
+    it('Sending with existing', function (done) {
+        Gift.give('foo', 'bar', function(data) {
+            expiration = new Date();
+            expiration.setDate(expiration.getDate() + 7);
+            assert.deepEqual(createNewGiftStub.getCall(0).args, ['foo', 'bar', expiration]);
+            assert.deepEqual(data, '{"Existing gifts":{"0":{}}}');
+            done();
+        });
+    });
+
+
 });
